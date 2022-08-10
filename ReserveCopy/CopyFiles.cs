271e6c0e4ed79906b6cc1d1a3446104004819411
prplmad <spacedo6pblu@gmail.com>
2022-08-10 16:13:47 +0300

    Init

---
+++ ReserveCopy/CopyFiles.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ReserveCopy
+{
+    public static class CopyFiles
+    {
+        public static void Copy(IConfigurationRoot configuration, NLog.Logger logger, string newFolderPath, string[] baseFolderPath, string newFolderPathForDeleting)
+        {
+            foreach (var baseFolder in baseFolderPath)
+            {
+                try
+                {
+                    if (!Directory.Exists(newFolderPath))
+                    {
+                        logger.Debug($"\nСоздаем папку {newFolderPath}");
+                    }
+                    Directory.CreateDirectory(newFolderPath);
+                    DirectoryInfo dirInfo = new DirectoryInfo(baseFolder);
+
+                    foreach (FileInfo file in dirInfo.GetFiles())
+                    {
+                        try
+                        {
+                            logger.Debug($"\nКопируем файл {file.Name}");
+                            File.Copy(file.FullName, newFolderPath + "\\" + file.Name, true);  // overwrite is true
+                            logger.Debug($"\nСкопирован файл {file.Name}");
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.Error($"\nНе удается скопировать файл {file.Name}\n Ошибка {ex.Message} \n Введите '1', чтобы пропустить этот файл и перейти к следующему, '2' - чтобы прервать процесс, '3' - чтобы прервать процесс и удалить уже скопированные файлы");
+                            while (true)
+                            {
+                                var input = Console.ReadLine();
+                                if (input == "1")
+                                {
+                                    goto End;
+                                }
+                                if (input == "2")
+                                {
+                                    return;
+                                }
+                                if (input == "3")
+                                {
+                                    Directory.Delete(newFolderPathForDeleting, true);
+                                    return;
+                                }
+                                else
+                                {
+                                    logger.Debug($"\nУдаляем директорию {newFolderPathForDeleting}");
+                                    Console.WriteLine("Введите '1', чтобы пропустить этот файл и перейти к следующему, '2' - чтобы прервать процесс, '3' - чтобы прервать процесс и удалить уже скопированные файлы");
+                                    continue;
+                                }
+                            }
+                        End: continue;
+
+                        }
+
+                    }
+                    foreach (string s in Directory.GetDirectories(baseFolder))
+                    {
+                        Copy(configuration, logger, newFolderPath + "\\" + Path.GetFileName(s), new[] { s }, newFolderPathForDeleting);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("\nВозникла ошибка " + ex.Message);
+                    throw;
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ ReserveCopy/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using ReserveCopy;
+using NLog;
+
+
+
+NLog.LogManager.Setup().LoadConfiguration(builder => {
+    builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToConsole();
+    builder.ForLogger().FilterMinLevel(LogLevel.Debug).WriteToFile(fileName: $"{DateTime.Now}.txt".Replace(':', '-'));
+});
+NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+IConfigurationRoot configuration = builder.Build();
+
+logger.Info("\nСтарт приложения");
+
+var newFolderPath = configuration.GetSection("Data:newFolder").Value + $"\\{DateTime.Now}".Replace(':', '-');
+var newFolderPathForDeleting = newFolderPath;
+var baseFolders = configuration.GetSection("Data:Base").Get<string[]>();
+CopyFiles.Copy(configuration, logger, newFolderPath, baseFolders , newFolderPathForDeleting);
+
+logger.Info("\nОкончание работы приложения");
+
+
